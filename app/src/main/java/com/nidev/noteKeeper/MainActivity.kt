4bce499a00d7a8c67ea1d6de3a014fd0ee8bc558
Nasif <nasifimtiaj.16@gmail.com>
2025-03-22 03:33:32 +0600

    Implemented to show splash screen.

--- app/src/main/java/com/nidev/noteKeeper/MainActivity.kt
+++ app/src/main/java/com/nidev/noteKeeper/MainActivity.kt
@@ -1,6 +1,22 @@
 package com.nidev.noteKeeper
 
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        splashScreen()
+    }
+
+    private fun splashScreen() {
+        val splashScreen = installSplashScreen()
+        splashScreen.setKeepOnScreenCondition { true }
+        Handler(Looper.getMainLooper()).postDelayed({
+            splashScreen.setKeepOnScreenCondition { false }
+        }, 1000)
+    }
 }
